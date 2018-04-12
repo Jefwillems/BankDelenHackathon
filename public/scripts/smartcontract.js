8753ae37e82eb8d10d17c33e8fb0a6d834854da0
Jefwillems <jefwillems100@gmail.com>
2018-04-12 13:41:36 +0200

    fixed bug

--- public/scripts/smartcontract.js
+++ public/scripts/smartcontract.js
@@ -1,38 +1,43 @@
 const contractAddress = "0x236215fB6cf1Aa8eC9320f95cE844Cb51EeE3171";
 
-
 //Adds recipient to the contract
 function addRecipient() {
-    console.log("adding recipient");
-    fetch('http://localhost:3000/api/recipients', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new FormData({
-        name: document.getElementById('name').value,
-        amount: document.getElementById('amount').value,
-        address: contractAddress
+  console.log("adding recipient");
+  fetch("http://localhost:3000/api/recipients", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      amount: document.getElementById("amount").value,
+      address: contractAddress
     })
-    }).then((response) => {
+  })
+    .then(response => {
       return response.json();
-    }).then((data) => {
+    })
+    .then(data => {
       console.log(data);
     })
     .catch(err => {
       console.log(err);
-    })
+    });
 }
 
 //Adds recipient to the contract
 function showRecipients() {
-    console.log(`fetching recipients and their amount of money${contractAddress}`)
-    fetch('http://localhost:3000/api/recipients/', {
-      method: 'get',
-    }).then(function(response) {
+  console.log(
+    `fetching recipients and their amount of money${contractAddress}`
+  );
+  fetch("http://localhost:3000/api/recipients/", {
+    method: "get"
+  })
+    .then(function(response) {
       return response.json();
       //add response to html
-    }).catch(err => {
-      console.log(err);
     })
+    .catch(err => {
+      console.log(err);
+    });
 }
