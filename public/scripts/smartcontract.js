76565e26372d41349e5a62391f601a6e811d5267
Ruben Alliet <allietruben@gmail.com>
2018-04-12 13:25:35 +0200

    500 status code for add recipient

--- public/scripts/smartcontract.js
+++ public/scripts/smartcontract.js
@@ -1,30 +1,38 @@
-const contractAddress = 0x236215fB6cf1Aa8eC9320f95cE844Cb51EeE3171;
-
+const contractAddress = "0x236215fB6cf1Aa8eC9320f95cE844Cb51EeE3171";
 
 
 //Adds recipient to the contract
 function addRecipient() {
-    console.log("adding recipient")
-    fetch('localhost:3000/api/recipients', {
+    console.log("adding recipient");
+    fetch('http://localhost:3000/api/recipients', {
       method: 'post',
-      body: JSON.stringify({
-          name: document.querySelector('#name'),
-          amount: document.querySelector('#amount'),
-          address: contractAddress
-      })
-    }).then(function(response) {
-      console.log("recipient succesfully added")
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new FormData({
+        name: document.getElementById('name').value,
+        amount: document.getElementById('amount').value,
+        address: contractAddress
+    })
+    }).then((response) => {
       return response.json();
+    }).then((data) => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
     })
 }
 
 //Adds recipient to the contract
 function showRecipients() {
     console.log(`fetching recipients and their amount of money${contractAddress}`)
-    fetch('localhost:3000/api/recipients/', {
+    fetch('http://localhost:3000/api/recipients/', {
       method: 'get',
     }).then(function(response) {
       return response.json();
       //add response to html
+    }).catch(err => {
+      console.log(err);
     })
 }
